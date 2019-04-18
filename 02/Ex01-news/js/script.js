7594275d38cfb5b4fa0dd6d934223b8147bad1a2
EsterLeal <e_esterribeiro@hotmail.com>
2019-04-17 22:20:14 -0300

    Eventos no JS- exercicio

--- 02/Ex01-news/js/script.js
+++ 02/Ex01-news/js/script.js
@@ -1,8 +1,40 @@
 var mensagem = document.querySelector('.mensagem');
 
 var botao = document.querySelector('.button');
-botao.addEventListener('click', function(evento){
+botao.addEventListener('click',function(evento){
     evento.preventDefault();
-    mensagem.textContent = 'Seu E-mail foi cadastrado com sucesso!! xD';
+
+    var email = document.querySelector('#newsInputEmail')
+    
+
+
+// Duas condições para saber se o email foi enviado
+
+// sE
+// if (true && true){
+    //     console.log('Verdade')
+// }
+
+// OU
+// if (true || true){
+//     console.log('Verdade')
+// }
+
+
+if (email.value == "" || email.value.length == 0){
+    mensagem.textContent = 'Digite um e-mail válido'
+
+
+}
+
+else if(email.value.indexOf('@')-1{
+    mensagem.textContent =  'O email informado é inválido'
+    return 
+}
+
+    mensagem.textContent = ' Seu E-mail ' + email.value + ' foi cadastrado com sucesso!! xD ';
+
 
 })
+
+// O indexOff busca a posição correspondente ao caractere @
---
+++ 02/Ex02-login/js/script.js
@@ -0,0 +1,5 @@
+// Usuário não vazio
+// tem que ter um @
+// senha de 6 caracteres
+// se ok - exibir mensagem - Acesso com sucesso
+// se falhar - exibir mensagem de erro
